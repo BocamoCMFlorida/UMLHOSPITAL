09c91e054c59bdf4adf1599d3266a5355e5443a3
BocamoCMFlorida <bocamo@alumnatflorida.es>
2025-04-03 11:45:38 +0200

    atributos ConsultaMedica

---
+++ untitled/src/ConsultaMedica.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class ConsultaMedica {
+    private Date fecha;
+    private String diagnostico;
+    private String tratamiento;
+    private Medico medico;
+    private Paciente paciente;
+}
