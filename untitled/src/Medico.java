f89afe5b3353ea7984bb4c6ff9c19fa9aa89a45d
BocamoCMFlorida <bocamo@alumnatflorida.es>
2025-04-03 11:41:26 +0200

    constructor

--- untitled/src/Medico.java
+++ untitled/src/Medico.java
@@ -2,4 +2,11 @@ public class Medico {
         private String nombre;
         private String numeroLicencia;
         private String especialidad;
+
+    public Medico(String nombre, String numeroLicencia, String especialidad) {
+        this.nombre = nombre;
+        this.numeroLicencia = numeroLicencia;
+        this.especialidad = especialidad;
+    }
+
 }
